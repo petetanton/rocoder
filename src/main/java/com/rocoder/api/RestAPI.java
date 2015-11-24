8a0455b6d2f59bdc8afba8af59ca697b0eb4d9fa
petetanton <pete.tanton@bbc.co.uk>
2015-11-24 17:02:20 +0000

    Initial commit

--- src/main/java/com/rocoder/api/RestAPI.java
+++ src/main/java/com/rocoder/api/RestAPI.java
@@ -16,5 +16,33 @@ public class RestAPI {
             return Response.status(403).entity("No token has been set or illegal token").build();
     }
 
+    @POST
+    @Path("/test")
+    public Response test(byte[] body, @HeaderParam("Content-Type") String contentType, @HeaderParam("key") String key) {
+        if (null == key)
+            return generateBadResponse(403, "No api key set in request");
+
+        String inputBody = new String(body);
+        String testJson = "{ \"status\":200, \"message\":\"okay\" }";
+        if (contentType.equals("application/json")) {
+            return generateResponse(200, testJson);
+        } else {
+            return generateBadResponse(404, "Unrecognised content-type");
+        }
+    }
+
+
+    private Response generateBadResponse(int status, String message) {
+        return generateResponse(status, "{\"status\":" + status + ",\"message\":\"" + message + "\"}");
+    }
+
+    private Response generateResponse(int status, String body) {
+        return Response.status(status).entity(body).header("Content-Type", "application/json").header("Server", "Rocoder").build();
+    }
+
+    private Response generateResponse(int status, String body, String contentType) {
+        return Response.status(status).entity(body).header("Content-Type", contentType).header("Server", "Rocoder").build();
+    }
+
 
 }
